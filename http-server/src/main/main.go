b86e7f0bab8acd6c4dff386380b43aa6f1495d94
supperdoggy <supperspggy229@gmail.com>
2020-08-17 19:22:57 +0300

    Solved hw

---
+++ http-server/src/main/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+func hello(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path != "/" {
+        http.Error(w, "404 not found.", http.StatusNotFound)
+        return
+    }
+
+    switch r.Method {
+    case "GET":
+        http.ServeFile(w, r, "form.html")
+    case "POST":
+        // Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
+        if err := r.ParseForm(); err != nil {
+            fmt.Fprintf(w, "ParseForm() err: %v", err)
+            return
+        }
+        name := r.FormValue("name")
+        surname := r.FormValue("surname")
+        age := r.FormValue("age")
+        fmt.Fprintf(w, "Name = %s\n", name)
+        fmt.Fprintf(w, "Surname = %s\n", surname)
+        fmt.Fprintf(w, "Age = %s\n", age)
+    }
+}
+
+func main() {
+    http.HandleFunc("/", hello)
+    fmt.Printf("Starting server...\n")
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+        log.Fatal(err)
+    }
+}
